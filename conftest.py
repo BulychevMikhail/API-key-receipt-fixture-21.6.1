e7955681f907c9004a689d3ba2f71f4e432350db
BulychevMikhail <102511691+BulychevMikhail@users.noreply.github.com>
2022-07-21 20:21:14 +0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,11 +1,19 @@
-# import pytest
-# from api import PetFriends
-# from settings import valid_email, valid_password, invalid_email, invalid_password, empty_email, empty_password
-#
-# @pytest.fixture()
-# def get_key():
-#     pf = PetFriends()
-#     status, key, _ = pf.get_api_key(valid_email, valid_password)
-#     assert status == 200
-#     assert 'key' in key
-#     return key
+import pytest
+import requests
+import requests_toolbelt
+from api import PetFriends
+from settings import valid_email, valid_password, invalid_email, invalid_password, empty_email, empty_password
+
+pf = PetFriends()
+
+@pytest.fixture()
+def get_api_key(email=valid_email, password=valid_password):
+    """ Проверяем что запрос api ключа возвращает статус 200 и в
+    тезультате содержится слово key"""
+    # Отправляем запрос и сохраняем полученный ответ с кодом статуса в status, а текст ответа в result
+    status, result = pf.get_api_key(email, password)
+
+    # Сверяем полученные данные с нашими ожиданиями
+    assert status == 200
+    assert 'key' in result
+    return result
