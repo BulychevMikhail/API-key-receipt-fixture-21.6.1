88401678de690076452b4a9c89042302173cd40f
BulychevMikhail <102511691+BulychevMikhail@users.noreply.github.com>
2022-07-21 20:20:03 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 
 
 valid_email = 'bulychev73@gmail.com'
-valid_password = 'Kott22011980'
+valid_password = '************'
 
 invalid_email = 'bulychev7333@gmail.com'
-invalid_password = 'KottKott22011980'
+invalid_password = '************'
 
 empty_email = ''
 empty_password = ''
